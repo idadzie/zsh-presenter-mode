a1aa3a19577a21a3b973981149143fde9da662eb
Isaac Dadzie <4581114+idadzie@users.noreply.github.com>
2020-10-22 20:32:55 +0000

    Add Tmux exception
    Tmux does not support backgroud color xterm control sequence. :cry:

--- term-emulator-bg-default.sh
+++ term-emulator-bg-default.sh
@@ -1,16 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -eo pipefail
 
 # https://superuser.com/questions/157563/programmatic-access-to-current-xterm-background-color
+
+if [[ -n $TMUX ]]; then
+  echo 'Tmux does not support backgroud color xterm control sequence.'
+  exit 1
+fi
 
 success=false
 exec < /dev/tty
 oldstty=$(stty -g)
 stty raw -echo min 0
-echo -en "\033]11;?\033\\" >/dev/tty
+echo -en "\033]11;?\033\\" > /dev/tty
 result=
-if IFS=';' read -r -d '\' color ; then
-    result=$(echo $color | sed 's/^.*\;//;s/[^rgb:0-9a-f/]//g')
-    success=true
+if IFS=';' read -r -d '\' color; then
+  result=$(echo $color | sed 's/^.*\;//;s/[^rgb:0-9a-f/]//g')
+  success=true
 fi
 stty $oldstty
 echo $result
