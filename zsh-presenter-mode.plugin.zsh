fe633fcfb3b0723d3a6714077834ea0c8428644d
Isaac Dadzie <4581114+idadzie@users.noreply.github.com>
2020-10-23 21:13:08 +0000

    Improve tmux support
    Set the background color for the active pane.
    
    Resolves: #1

--- zsh-presenter-mode.plugin.zsh
+++ zsh-presenter-mode.plugin.zsh
@@ -10,7 +10,7 @@ typeset -g TERM_EMULATOR_BG_DEFAULT
 if [[ -z "$TMUX" ]]; then
   : ${TERM_EMULATOR_BG_DEFAULT:=$(source ${0:h}/bin/get-terminal-emulator-bg-color)}
 elif [[ -n "$TMUX" ]]; then
-  : ${TERM_EMULATOR_BG_DEFAULT:='rgb:2828/2828/2828'}
+  : ${TERM_EMULATOR_BG_DEFAULT:="#282828"}
 fi
 
 export TERM_EMULATOR_BG_DEFAULT=${~TERM_EMULATOR_BG_DEFAULT}
@@ -28,8 +28,14 @@ function presenter_mode_start() {
   echo "Backed up preexec array: $Z_PRESENTER_PREEXEC_FUNCTIONS_BACKUP"
   preexec_functions+=(preshow_expanded_command)
   Z_PRESENTER_MODE_ENABLED=1
-  # Set terminal background to black to improve contrast:
-  echo -e "\033]11;#000000\a"
+
+  # Set terminal background to black to improve contrast
+  if [[ -n "$TMUX" ]]; then
+    tmux select-pane -P 'bg=colour232'
+  else
+    echo -e "\033]11;#000000\a"
+  fi
+
   # Switch toggle command
   alias presenter-toggle=presenter_mode_stop
   zle -N toggle-presenter-mode presenter_mode_stop_widget
@@ -40,8 +46,15 @@ function presenter_mode_start() {
   printf "%*s\n" $(((${#title}+$(tput cols))/2)) "$title"
   echo ""
 }
+
 function presenter_mode_stop() {
-  echo -e "\033]11;${TERM_EMULATOR_BG_DEFAULT}\a"
+  # Restore background color
+  if [[ -n "$TMUX" ]]; then
+    tmux select-pane -P "bg=${TERM_EMULATOR_BG_DEFAULT}"
+  else
+    echo -e "\033]11;${TERM_EMULATOR_BG_DEFAULT}\a"
+  fi
+
   # Check that we're not gonna accidentally set the preexec array blank
   if (( $+Z_PRESENTER_MODE_ENABLED )) && (( $+Z_PRESENTER_PREEXEC_FUNCTIONS_BACKUP )); then
     preexec_functions=($Z_PRESENTER_PREEXEC_FUNCTIONS_BACKUP)
