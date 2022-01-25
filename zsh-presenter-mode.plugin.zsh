659c9cebc9c420046fc53afe5c8d3118de3cc968
Isaac Dadzie <4581114+idadzie@users.noreply.github.com>
2022-01-25 11:35:53 +0000

    Remove dead link

--- zsh-presenter-mode.plugin.zsh
+++ zsh-presenter-mode.plugin.zsh
@@ -1,6 +1,3 @@
-# According to the Zsh Plugin Standard:
-# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
-
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 
 # Then ${0:h} to get plugin's directory
